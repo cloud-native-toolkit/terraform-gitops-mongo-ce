5b2628e49d5dbfaa54a517dfc7467ddd194f937a
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-02-10 03:39:26 -0600

    Fixes create-yaml script
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- scripts/create-yaml.sh
+++ scripts/create-yaml.sh
@@ -13,5 +13,5 @@ mkdir -p "${DEST_DIR}"
 cp -R "${CHART_DIR}/"* "${DEST_DIR}"
 
 if [[ -n "${VALUES_CONTENT}" ]]; then
-  echo "${VALUES_CONTENT}" > "${DEST_DIR}"
+  echo "${VALUES_CONTENT}" > "${DEST_DIR}/values.yaml"
 fi
