038ca27422f192c4b2f3f6be98c34acd7df9d565
Sean Sundberg <seansund@us.ibm.com>
2022-07-27 12:44:08 -0500

    Switches back from terraform provider
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,79 +1,85 @@
 
 output "name" {
   description = "The name of the module"
-  value       = gitops_module.module.name
+  value       = local.name
+  depends_on  = [null_resource.setup_gitops]
 }
 
 output "branch" {
   description = "The branch where the module config has been placed"
-  value       = gitops_module.module.branch
+  value       = local.application_branch
+  depends_on  = [null_resource.setup_gitops]
 }
 
 output "namespace" {
   description = "The namespace where the module will be deployed"
-  value       = gitops_module.module.namespace
+  value       = local.namespace
+  depends_on  = [null_resource.setup_gitops]
 }
 
 output "server_name" {
   description = "The server where the module will be deployed"
-  value       = gitops_module.module.server_name
+  value       = var.server_name
+  depends_on  = [null_resource.setup_gitops]
 }
 
 output "layer" {
   description = "The layer where the module is deployed"
-  value       = gitops_module.module.layer
+  value       = local.layer
+  depends_on  = [null_resource.setup_gitops]
 }
 
 output "type" {
   description = "The type of module where the module is deployed"
-  value       = gitops_module.module.type
+  value       = local.type
+  depends_on  = [null_resource.setup_gitops]
 }
 
 output "username" {
   value       = local.username
   description = "mongo admin user"
-  depends_on  = [gitops_module.module]
+  depends_on  = [null_resource.setup_gitops]
 }
 
 output "password" {
   value       = var.password
   description = "mongo admin password"
-  depends_on  = [gitops_module.module]
+  depends_on  = [null_resource.setup_gitops]
 }
 
 output "port" {
   value       = var.port
   description = "mongo admin pw"
-  depends_on  = [gitops_module.module]
+  depends_on  = [null_resource.setup_gitops]
   sensitive   = true
 }
 
 output "svcname" {
   value       = var.service_name
   description = "mongo service name"
-  depends_on  = [gitops_module.module]
+  depends_on  = [null_resource.setup_gitops]
   sensitive   = true
 }
 
 output "private_svc_host" {
-  value       = "${var.service_name}.${gitops_module.module.namespace}.svc"
+  value       = "${var.service_name}.${local.namespace}.svc"
   description = "Host name for private service endpoint"
 }
 
 output "private_master_host" {
-  value       = "${var.service_name}-0.${var.service_name}.${gitops_module.module.namespace}.svc"
+  value       = "${var.service_name}-0.${var.service_name}.${local.namespace}.svc"
   description = "Host name for private master instance endpoint"
 }
 
 output "private_replica_host" {
-  value       = [ for index in range(var.replicaset_count): "${var.service_name}-${index}.${var.service_name}.${gitops_module.module.namespace}.svc" ]
+  value       = [ for index in range(var.replicaset_count): "${var.service_name}-${index}.${var.service_name}.${local.namespace}.svc" ]
   description = "Host names for private replica instance endpoints"
 }
 
 output "replicaset_count" {
   value       = var.replicaset_count
   description = "Count of replicaset count"
-  depends_on  = [gitops_module.module]
+  depends_on  = [null_resource.setup_gitops]
   sensitive   = true
 }
 
